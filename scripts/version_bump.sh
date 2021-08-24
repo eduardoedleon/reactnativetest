7839554f91223ef6d22b2f10cb742f9a58ec2fe4
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-24 15:25:04 +1200

    fixed version bump script

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]
-  then
-    echo "Error: No version type supplied"
-    exit 1
-fi
-
-npm version $1
 (cd android && fastlane version_bump)
 
 git add android/app/build.gradle package.json
