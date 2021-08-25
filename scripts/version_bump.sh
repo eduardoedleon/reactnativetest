34d2dbbb5d449ca5ce37294cf8c6fa648fb38e9d
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-25 16:53:41 +1200

    added fastlane plugins

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-(cd android && gem install fastlane-plugin-firebase_app_distribution)
-(cd android && gem install fastlane-plugin-increment_version_name)
-(cd android && gem install fastlane-plugin-increment_version_code)
-(cd android && gem install fastlane-plugin-load_json)
+(cd android && gem install fastlane-plugin-firebase_app_distribution --user-install)
+(cd android && gem install fastlane-plugin-increment_version_name --user-install)
+(cd android && gem install fastlane-plugin-increment_version_code --user-install)
+(cd android && gem install fastlane-plugin-load_json --user-install)
 
 (cd android && fastlane version_bump)
 
