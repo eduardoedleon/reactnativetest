832b40d9900e5ce1f5edb9ace6a374ffd09072d2
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-30 15:31:38 +1200

    fixed version bump job

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
+(cd android && gem install fastlane-plugin-increment_version_name)
+(cd android && gem install fastlane-plugin-increment_version_code)
+(cd android && gem install fastlane-plugin-load_json)
 
-gem install fastlane-plugin-increment_version_name
-gem install fastlane-plugin-increment_version_code
-gem install fastlane-plugin-load_json
-
-bundle exec fastlane version_bump
+(cd android && bundle exec fastlane version_bump)
 
 git config --local user.email $USER_EMAIL
 git config --local user.name $USER_NAME
