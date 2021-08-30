64095bab887176dd7c92005cb2e02bb0da34a961
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-30 14:07:46 +1200

    fixed version bump job

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
-
-gem install fastlane-plugin-firebase_app_distribution
-gem install fastlane-plugin-increment_version_name
-gem install fastlane-plugin-increment_version_code
-gem install fastlane-plugin-load_json
+(cd android && gem install fastlane-plugin-firebase_app_distribution)
+(cd android && gem install fastlane-plugin-increment_version_name)
+(cd android && gem install fastlane-plugin-increment_version_code)
+(cd android && gem install fastlane-plugin-load_json)
 
 (cd android && bundle exec fastlane version_bump)
 
 git config --local user.email $USER_EMAIL
 git config --local user.name $USER_NAME
 
-git add app/build.gradle ../package.json
+git add android/app/build.gradle package.json
