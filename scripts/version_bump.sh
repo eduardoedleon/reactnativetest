04d535878758860dc2adcdc469a1f28be274df1d
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-30 18:18:46 +1200

    deleted ruby cache and replace with macos which include fastlane

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -3,8 +3,8 @@ set -e
 
 EXTRACTED_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
 
-export VERSION_CODE=${{ github.run_number }}
-export VERSION_NAME="$EXTRACTED_VERSION-${{ github.run_number }}"
+export VERSION_CODE=$github.run_number
+export VERSION_NAME="$EXTRACTED_VERSION-$github.run_number"
 
 #(cd android && bundle exec fastlane version_bump)
 
