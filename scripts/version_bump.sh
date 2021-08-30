bb7b97151dec32b1729be590d1469601d0e0f6ff
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-30 18:10:16 +1200

    added env variables for version code and version name

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-(cd android && bundle exec fastlane version_bump)
+EXTRACTED_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
+
+export VERSION_CODE=${{ github.run_number }}
+export VERSION_NAME="$EXTRACTED_VERSION-${{ github.run_number }}"
+
+#(cd android && bundle exec fastlane version_bump)
 
 git config --local user.email $USER_EMAIL
 git config --local user.name $USER_NAME
