1a5fd7c1c99fa0e16ea33dbc8067e22144751a77
Eduardo D'Leon <eduardo.dleon@fronde.com>
2021-08-31 12:19:21 +1200

    added version bump in ios release ci

--- scripts/version_bump.sh
+++ scripts/version_bump.sh
@@ -6,9 +6,12 @@ EXTRACTED_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.js
 export VERSION_CODE=$github.run_number
 export VERSION_NAME="$EXTRACTED_VERSION-$github.run_number"
 
-#(cd android && bundle exec fastlane version_bump)
-
 git config --local user.email $USER_EMAIL
 git config --local user.name $USER_NAME
 
-git add android/app/build.gradle
+if ["$PLATFORM_OS" == "ios"]
+then
+  cd "${GITHUB_WORKSPACE}/ios/"
+  agvtool new-marketing-version "$VERSION_NAME"
+  agvtool new-version -all "$VERSION_CODE"
+fi
